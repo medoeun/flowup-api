972864c03c0b540446d2f6945fd89badc230c2f8
Doeun <id5eun30@gmail.com>
2024-09-17 15:09:00 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/flowup/api/FlowUpApplication.java
@@ -0,0 +1,12 @@
+package com.flowup.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowUpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/flowup/api/FlowUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowup.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

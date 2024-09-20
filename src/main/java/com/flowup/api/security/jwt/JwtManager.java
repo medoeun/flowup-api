d60171d2ed0c864ce706055c71bd5cfb0eb7d380
Doeun <id5eun30@gmail.com>
2024-09-20 19:32:50 +0900

    feat: UserRepository 작성, JwtManager 작성 (#18)

---
+++ src/main/java/com/flowup/api/security/jwt/JwtManager.java
@@ -0,0 +1,94 @@
+package com.flowup.api.security.jwt;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class JwtManager {
+	private final Key secretKey;
+
+	@Value("${jwt.token-validity-in-seconds}")
+	private long tokenValidityInseconds;
+
+	public JwtManager(@Value("${jwt.secret}") String secret) {
+		byte[] keyBytes = Base64.getDecoder().decode(secret);
+		this.secretKey = new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
+	}
+
+	// JWT 토큰 생성
+	public String generateToken(Authentication authentication) {
+		CustomUserDetails userDetails = (CustomUserDetails)authentication.getPrincipal();
+		Long id = userDetails.getMemberId();
+
+		// JWT 토큰에 최소한의 정보(페이로드)만 포함
+		Claims claims = Jwts.claims().setSubject(String.valueOf(id));
+
+		Date now = new Date();
+		Date validity = new Date(now.getTime() + tokenValidityInseconds * 1000);
+
+		return Jwts.builder()
+			.setClaims(claims)
+			.setIssuedAt(now)
+			.setExpiration(validity)
+			.signWith(secretKey, SignatureAlgorithm.HS256)
+			.compact();
+	}
+
+	// HTTP 요청 헤더에서 JWT 토큰을 추출
+	public String resolveToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader("Authorization");
+		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);  // "Bearer " 이후의 토큰 실제부분만 추출
+		}
+		return null;
+	}
+
+	public Authentication getAuthentication(String token, UserDetails userDetails) {
+		return new UsernamePasswordAuthenticationToken(
+			userDetails, null, userDetails.getAuthorities());
+	}
+
+	public Long getUserId(String token) {
+		return Long.parseLong(Jwts.parserBuilder()
+			.setSigningKey(secretKey)
+			.build()
+			.parseClaimsJws(token)
+			.getBody()
+			.getSubject());
+	}
+
+	// JWT 토큰 유효성 검증
+	public boolean isValidToken(String token) {
+		try {
+			Jwts.parserBuilder()
+				.setSigningKey(secretKey)
+				.build()
+				.parseClaimsJws(token);
+			return true;
+		} catch (ExpiredJwtException e) {
+			log.info("Expired JWT token: {}", token);
+			return false;
+		} catch (JwtException | IllegalArgumentException e) {
+			log.info("Invalid JWT token: {}", token);
+			return false;
+		}
+	}
+}

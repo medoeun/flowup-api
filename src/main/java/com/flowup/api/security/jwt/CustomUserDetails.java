f6a03f46e02b36f72882557c2dc0b3fa7f06eb62
Doeun <id5eun30@gmail.com>
2024-09-20 19:33:55 +0900

    feat: 인증 필터 및 CustomUserDetails 구현 (#18)

---
+++ src/main/java/com/flowup/api/security/jwt/CustomUserDetails.java
@@ -0,0 +1,59 @@
+package com.flowup.api.security.jwt;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.flowup.api.user.entity.User;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+	private final User user;
+
+	public CustomUserDetails(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// ADMIN 또는 MEMBER 권한을 GrantedAuthority로 변환
+		return Collections.singletonList(new SimpleGrantedAuthority(user.getAuthority().name()));
+	}
+
+	public Long getMemberId() {
+		return user.getId();
+	}
+}
